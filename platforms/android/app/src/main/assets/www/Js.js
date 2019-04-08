ba2c038e4dcc28da383e1722d4e7b43d000d7a41
K-fuzz <43284360+K-fuzz@users.noreply.github.com>
2019-04-08 17:20:29 +0200

    TT2 v.2
    TT2 v.2

--- platforms/android/app/src/main/assets/www/Js.js
+++ platforms/android/app/src/main/assets/www/Js.js
@@ -1,34 +1,44 @@
  document.addEventListener('deviceready', ready)
 
- function ready() {
- 	document.getElementById('Abrir').addEventListener("click", createFile)
- 	document.getElementById('Leer').addEventListener("click", readFile)
- 	document.getElementById('Escribir').addEventListener("click", writeFile)
- 	document.getElementById('Borrar').addEventListener("click", removeFile)
 
- 	console.log(cordova.file)
+ function ready() {console.log(cordova.file)
 
- }
- var valued
- function directorio(){
- 		var selector = document.getElementById('directorio');
- 		var valued = selector[selector.selectedIndex].value;
- 		alert(valued);}
- 	
- var nombre = getElementById('nombre').value
+  var selector
+  var ruta
+  var fileName
 
- function createFile(){
-		window.resolveLocalFileSystemURL(valued+".PERSISTENT", 0, function (fs) {
 
-    	console.log('file system open: ' + fs.name);
-    	fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, 
-    	function (fileEntry) {
-		console.log("fileEntry is file?" + fileEntry.isFile.toString());
-        fileEntry.name == nombre
-        fileEntry.fullPath == 'downloads/someFile.txt'
-        writeFile(fileEntry, null);
 
-    	}, onErrorCreateFile);
+ function directorios(){
 
-		}, onErrorLoadFs);
-		}
+        var selector = document.getElementById('selected')
+        var ruta = selector[selector.selectedIndex].value;
+        var fileName = document.getElementById('nombre').value
+
+
+        if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
+        else if (ruta == "cordova.file.dataDirectory"){ruta = cordova.file.dataDirectory}
+        else if (ruta == "cordova.file.externalRootDirectory"){ ruta = cordova.file.externalRootDirectory}
+        else if (ruta == "cordova.file.externalCacheDirectory"){ ruta = cordova.file.externalCacheDirectory}
+        else if (ruta == "cordova.file.externalDataDirectory"){ ruta = cordova.file.externalDataDirectory}
+
+ 		};
+
+
+function create(){
+
+        directorios();
+
+        window.resolveLocalFileSystemURL(ruta, function (dirEntry){
+        alert('file system create: '+ dirEntry.name);
+        dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){alert("Creado");}
+        ,onErrorLoadFs)}  )};
+
+
+
+
+function onErrorLoadFs(){alert("Error fs")}
+function onErrorCreateFile(){alert("Error Create")}
+
+}
+ 
--- www/Js.js
+++ www/Js.js
@@ -1,68 +1,41 @@
  document.addEventListener('deviceready', ready)
 
 
- function ready() {
- 	document.getElementById('Abrir').addEventListener("click", createFile)
- 	document.getElementById('Leer').addEventListener("click", readFile)
- 	document.getElementById('Escribir').addEventListener("click", writeFile)
- 	document.getElementById('Borrar').addEventListener("click", removeFile)
+ function ready() {console.log(cordova.file)
 
- 	console.log(cordova.file)
-    
- }
-
+  var selector
   var ruta
-  var nombre 
+  var fileName
 
 
- function directorio(){
-    var nombre = getElementById('nombre').value
 
- 		var selector = document.getElementById('directorio');
- 		var ruta = selector[selector.selectedIndex].value;
+ 	function directorios(){
+
+        var selector = document.getElementById('selected')
+        var ruta = selector[selector.selectedIndex].value;
+        var fileName = document.getElementById('nombre').value
+
+
         if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
         else if (ruta == "cordova.file.dataDirectory"){ruta = cordova.file.dataDirectory}
         else if (ruta == "cordova.file.externalRootDirectory"){ ruta = cordova.file.externalRootDirectory}
         else if (ruta == "cordova.file.externalCacheDirectory"){ ruta = cordova.file.externalCacheDirectory}
         else if (ruta == "cordova.file.externalDataDirectory"){ ruta = cordova.file.externalDataDirectory}
+
  		};
- 	
- var fileName = getElementById('nombre').value;
- var texto 
 
 
-function createFile(dirEntry, fileName, isAppend) {
-    
-    dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+	function crear(){
 
-        writeFile(fileEntry, null, isAppend); }, onErrorCreateFile); };
+        directorios();
 
+        window.resolveLocalFileSystemURL(ruta, function (dirEntry){
+        alert('file system create: '+ dirEntry.name);
+        dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){alert("Creado");}
+        ,onErrorLoadFs)}  )};
 
-function writeFile(fileEntry, dataObj) {
-    
-    fileEntry.createWriter(function (fileWriter) {
+		function onErrorLoadFs(){alert("Error fs")}
+		function onErrorCreateFile(){alert("Error Create")}
 
-        fileWriter.onwriteend = function() {
-            console.log("Successful file write...");
-            readFile(fileEntry);
-        };
-
-        fileWriter.onerror = function (e) {
-            console.log("Failed file write: " + e.toString());
-        };
-
-        texto = document.getElementById('texto').value
-
-        fileWriter.write(texto);
-    });
-}
-
-function onErrorLoadFs(){alert("Error fs")}
-function onErrorCreateFile(){alert("error Create")}
-
-
- function create(){
-		window.resolveLocalFileSystemURL(ruta, function (dirEntry){
-            console.log('file system create: '+ dirEntry.name);
-            createFile(dirEntry, fileName, isAppend);
-            }, onErrorLoadFs); }
+			};
+ 
