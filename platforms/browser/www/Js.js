cc137b4de8d22f6e9b19084df0118c01a30c5f90
K-fuzz <43284360+K-fuzz@users.noreply.github.com>
2019-04-08 19:29:25 +0200

    TT2 v.3

--- platforms/browser/www/Js.js
+++ platforms/browser/www/Js.js
@@ -3,17 +3,18 @@
 
  function ready() {console.log(cordova.file)
 
+  
+};
+  var filename
+  var ruta 
   var selector
-  var ruta
-  var fileName
 
 
-
- function directorios(){
-
-        var selector = document.getElementById('selected')
-        var ruta = selector[selector.selectedIndex].value;
-        var fileName = document.getElementById('nombre').value
+	function crear(){
+		filename = document.getElementById('nombre').value
+		selector = document.getElementById('selected')
+         ruta = selector[selector.selectedIndex].value;
+         fileName = document.getElementById('nombre').value
 
 
         if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
@@ -22,23 +23,26 @@
         else if (ruta == "cordova.file.externalCacheDirectory"){ ruta = cordova.file.externalCacheDirectory}
         else if (ruta == "cordova.file.externalDataDirectory"){ ruta = cordova.file.externalDataDirectory}
 
- 		};
+        window.resolveLocalFileSystemURL(ruta, resolveok, function e (){alert("Error")}) };
+
+    function resolveok(directorio){
+    	alert(directorio);
+    	directorio.getFile('prueba.txt',{create: true, exclusive: true}, 
+
+    	function ok(){alert("El archivo se ha creado correctamente")} , function notok(){alert("Ha ocurrido un error al crear el archivo.")})
 
 
-function create(){
-
-        directorios();
-
-        window.resolveLocalFileSystemURL(ruta, function (dirEntry){
-        alert('file system create: '+ dirEntry.name);
-        dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){alert("Creado");}
-        ,onErrorLoadFs)}  )};
+    };
 
 
 
 
-function onErrorLoadFs(){alert("Error fs")}
-function onErrorCreateFile(){alert("Error Create")}
 
-}
+
+
+
+		
+			
+
+
  
--- www/Js.js
+++ www/Js.js
@@ -6,12 +6,24 @@
   
 };
   var filename
+  var ruta 
+  var selector
 
 
 	function crear(){
 		filename = document.getElementById('nombre').value
+		selector = document.getElementById('selected')
+         ruta = selector[selector.selectedIndex].value;
+         fileName = document.getElementById('nombre').value
 
-        window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, resolveok, function e (){alert("Error")}) };
+
+        if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
+        else if (ruta == "cordova.file.dataDirectory"){ruta = cordova.file.dataDirectory}
+        else if (ruta == "cordova.file.externalRootDirectory"){ ruta = cordova.file.externalRootDirectory}
+        else if (ruta == "cordova.file.externalCacheDirectory"){ ruta = cordova.file.externalCacheDirectory}
+        else if (ruta == "cordova.file.externalDataDirectory"){ ruta = cordova.file.externalDataDirectory}
+
+        window.resolveLocalFileSystemURL(ruta, resolveok, function e (){alert("Error")}) };
 
     function resolveok(directorio){
     	alert(directorio);
