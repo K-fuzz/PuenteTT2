39c5b1ff5bdff93c0550e49347cf482002fae9b5
K-fuzz <43284360+K-fuzz@users.noreply.github.com>
2019-04-08 18:23:18 +0200

    Update Js.js

--- www/Js.js
+++ www/Js.js
@@ -5,32 +5,19 @@
 
   
 };
-  var selector
-  var ruta
-  var fileName
-
- 	function directorios(){
-
-         selector = document.getElementById('selected')
-         ruta = selector[selector.selectedIndex].value;
-         fileName = document.getElementById('nombre').value
-
-
-        if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
-        else if (ruta == "cordova.file.dataDirectory"){ruta = cordova.file.dataDirectory}
-        else if (ruta == "cordova.file.externalRootDirectory"){ ruta = cordova.file.externalRootDirectory}
-        else if (ruta == "cordova.file.externalCacheDirectory"){ ruta = cordova.file.externalCacheDirectory}
-        else if (ruta == "cordova.file.externalDataDirectory"){ ruta = cordova.file.externalDataDirectory}
-
- 		};
+  
 
 
 	function crear(){
 		alert("PRUEBA")
 
-        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry){alert('Directorio: '+ dirEntry.name);
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, 
 
-        dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){alert("Creado");}
+        function (dirEntry){alert('Directorio: '+ dirEntry.name);
+        dirEntry.getFile('prueba.txt', {create: true, exclusive: false}, 
+
+        function(fileEntry){alert("Creado");}
+
         ,function onErrorLoadFs(){alert("Error fs")})}  )};
 
 
