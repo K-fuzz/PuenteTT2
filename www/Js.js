5d1072a83ca6b3daf1bba7436e5679554afde588
K-fuzz <43284360+K-fuzz@users.noreply.github.com>
2019-04-08 19:15:59 +0200

    Update Js.js

--- www/Js.js
+++ www/Js.js
@@ -5,20 +5,19 @@
 
   
 };
-  
+  var filename
 
 
 	function crear(){
-		alert("PRUEBA")
-		
+		filename = document.getElementById('nombre').value
 
-        window.resolveLocalFileSystemURL(cordova.file.dataDirectory, resolveok, function e (){alert("Error")}) };
+        window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, resolveok, function e (){alert("Error")}) };
 
     function resolveok(directorio){
     	alert(directorio);
     	directorio.getFile('prueba.txt',{create: true, exclusive: true}, 
 
-    	function ok(){alert("Bien")} , function notok(){alert("mal")})
+    	function ok(){alert("El archivo se ha creado correctamente")} , function notok(){alert("Ha ocurrido un error al crear el archivo.")})
 
 
     };
