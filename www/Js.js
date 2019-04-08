34aa0fe96ec0c3d37b89ef9d6a35a8011cd9a4a2
K-fuzz <43284360+K-fuzz@users.noreply.github.com>
2019-04-08 18:04:33 +0200

    Update Js.js

--- www/Js.js
+++ www/Js.js
@@ -3,17 +3,17 @@
 
  function ready() {console.log(cordova.file)
 
+  
+};
   var selector
   var ruta
   var fileName
 
-
-
  	function directorios(){
 
-        var selector = document.getElementById('selected')
-        var ruta = selector[selector.selectedIndex].value;
-        var fileName = document.getElementById('nombre').value
+         selector = document.getElementById('selected')
+         ruta = selector[selector.selectedIndex].value;
+         fileName = document.getElementById('nombre').value
 
 
         if (ruta == "cordova.file.cacheDirectory"){ruta = cordova.file.cacheDirectory}
@@ -26,16 +26,17 @@
 
 
 	function crear(){
+		alert("PRUEBA")
 
         directorios();
 
-        window.resolveLocalFileSystemURL(ruta, function (dirEntry){
-        alert('file system create: '+ dirEntry.name);
+        window.resolveLocalFileSystemURL(ruta, function (dirEntry){alert('fichero creado: '+ dirEntry.name);
+
         dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){alert("Creado");}
-        ,onErrorLoadFs)}  )};
+        ,function onErrorLoadFs(){alert("Error fs")})}  )};
 
-		function onErrorLoadFs(){alert("Error fs")}
-		function onErrorCreateFile(){alert("Error Create")}
+		
+			
 
-			};
+
  
